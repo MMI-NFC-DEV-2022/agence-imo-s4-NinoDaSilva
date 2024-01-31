517788fe83db623fd928554309360accdb22a15d
Nino DA SILVA <ninoportos@gmail.com>
2024-01-31 14:48:00 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Test Agence imo</h1>
+
+    <nav class="*:text-red-500 flex flex-col">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <Suspense>
+      <RouterView />
+    </Suspense>
+  </main>
+
+  <footer>
+    
+  </footer>
+</template>
---
+++ src/pages/offres/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { allMaisonsSorted } from '@/backend'
+const maisonsListe = await allMaisonsSorted()
+</script>
+
+<template>
+    <ul>
+        <li v-for="uneMaison of maisonsListe" :v-key="uneMaison.id" v-bind="{ ...uneMaison }">
+          <RouterLink
+            :to="{
+              name: 'offres-id',
+              params: {
+                id: uneMaison.id
+              }
+            }"
+            class="text-red-400 hover:text-red-600"
+          >
+            {{ uneMaison.nomMaison }}
+          </RouterLink>
+        </li>
+      </ul>
+</template>
