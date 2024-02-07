50328296836ea7df7048da5b5df1778d4e86a738
Nino DA SILVA <ninoportos@gmail.com>
2024-02-07 09:33:59 +0100

    test supabase console

--- src/pages/liste-supabase.vue
+++ src/pages/liste-supabase.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import AfficheMaison from '@/components/AfficheMaison.vue';
 import { type SchemaOffreMaison } from '@/types';
-import { supabase } from "../supabase";
-console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+import { supabase } from "@/supabase";
 
 const tableauMaisons:SchemaOffreMaison[] = [];
+
+
 </script>
 
 
--- src/supabase.ts
+++ src/supabase.ts
@@ -4,3 +4,5 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
